98ca0c156f8f52a1b2faedaa18c499ac6367c3c9
Jon <lundbyjon@hotmail.com>
2023-03-15 09:39:56 +0100

    dark/light working

--- app.js
+++ app.js
@@ -3,11 +3,26 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
-    //event listener for select on HTML
-    document.querySelector("#select-color-mode").addEventListener("change", modeSelected);
+  //event listener for select on HTML
+  document.querySelector("#select-color-mode").addEventListener("change", modeSelected);
 }
 
 // modeSelected called when #select-color-mode changes value (the user select color mode)
 function modeSelected() {
-    console.log("New color mode selected");
+  const selectedColorMode = this.value;
+  console.log(selectedColorMode);
+
+  changeMode(selectedColorMode);
+}
+
+function changeMode(mode) {
+  console.log("changemode called: " + mode);
+
+  if (mode == "dark") {
+    document.querySelector("body").classList.add("dark-mode");
+  } else if (mode == "light") {
+    document.querySelector("body").classList.remove("dark-mode");
+    }
+    
+
 }
