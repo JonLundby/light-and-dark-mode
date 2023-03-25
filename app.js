1d4f35388be8d6d4959df2547982a90870a8fb9f
Jon <lundbyjon@hotmail.com>
2023-03-25 15:09:06 +0100

    complete

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ window.addEventListener("load", initApp);
 function initApp() {
   //event listener for select on HTML
   document.querySelector("#select-color-mode").addEventListener("change", modeSelected);
+  detectColorMode();
+  quiz();
+}
+
+function quiz() {
+  const answer = prompt("what is the original name of javascript?");
+  if (answer == "ECMAScript") {
+    alert("yes, that is correct")
+  } else {
+    alert("wrong... it is ECMAScript!")
+  }
 }
 
 // modeSelected called when #select-color-mode changes value (the user select color mode)
@@ -13,16 +24,49 @@ function modeSelected() {
   console.log(selectedColorMode);
 
   changeMode(selectedColorMode);
+  saveUserColorMode(selectedColorMode);
 }
 
 function changeMode(mode) {
   console.log("changemode called: " + mode);
+  const bod = document.querySelector("body");
 
-  if (mode == "dark") {
-    document.querySelector("body").classList.add("dark-mode");
-  } else if (mode == "light") {
-    document.querySelector("body").classList.remove("dark-mode");
-    }
-    
+  if (mode === "light") {
+    resetColorMode();
+    bod.classList.add("body");
+  } else if (mode === "dark") {
+    resetColorMode();
+    bod.classList.add("dark-mode");
+  } else if (mode === "green") {
+    resetColorMode();
+    bod.classList.add("green-mode");
+  } else if (mode === "yellow") {
+    resetColorMode();
+    bod.classList.add("yellow-mode");
+  }
+}
 
+function resetColorMode() {
+  console.log("color reset...");
+  document.querySelector("body").classList.remove("dark-mode", "green-mode", "yellow-mode");
+}
+
+function saveUserColorMode(mode) {
+  localStorage.setItem("savedTheme", mode);
+}
+
+function readColorMode() {
+  const savedTheme = localStorage.getItem("savedTheme");
+  console.log("reading..." + savedTheme);
+  return savedTheme;
+}
+
+function detectColorMode() {
+  const modeFromLocalStorage = readColorMode();
+  console.log("detecting... " + modeFromLocalStorage);
+
+  if (modeFromLocalStorage) {
+    document.querySelector("#select-color-mode").value = modeFromLocalStorage;
+    changeMode(modeFromLocalStorage);
+  }
 }
